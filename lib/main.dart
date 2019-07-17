2098c2e068cf18462bb5789d13650e82de094f7f
Sumit Kumar <sumitskl1999@gmail.com>
2019-07-17 23:54:34 +0530

    Inits Files

---
+++ lib/main.dart
@@ -0,0 +1,111 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Dashboard(),
+    );
+  }
+}
+
+class Dashboard extends StatefulWidget {
+  @override
+  _DashboardState createState() => _DashboardState();
+}
+
+class _DashboardState extends State<Dashboard> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Color(0xFF0d1839),
+      body: SafeArea(
+        child: Column(
+          children: <Widget>[
+            Container(
+              margin: const EdgeInsets.all(10.0),
+              alignment: Alignment.topCenter,
+              child: Text(
+                'Explore',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 40,
+
+                ),
+              ),
+            ),
+            Container(
+              height: 50,
+            ),
+            Container(
+              width: 370,
+              alignment: Alignment.centerLeft,
+              child: Text(
+                'Trending',
+                style: TextStyle(
+                  fontSize: 25,
+                  color: Colors.white,
+                ),
+
+              ),
+            ),
+            Container(
+              height: 10,
+            ),
+            Container(
+              width: 390,
+              height: 260,
+              child: Stack(
+                children: <Widget>[
+
+                  new Align(alignment: Alignment.centerLeft,
+                    child : Container(
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(8),
+                        color: Color(0xFF485fa9),
+                        boxShadow: [
+                          new BoxShadow(
+                              color: Color(0x4D000000),
+                              offset: new Offset(0.0, 5.0),
+                              blurRadius: 20.0,
+                              spreadRadius: 1)
+                        ],//gitkraken khol abb
+                        
+                      ),
+                      height: 180,
+                      width: 360,
+
+                    ),),
+
+                  new Align(alignment: Alignment.centerLeft,
+                  child : Container(
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(8),
+                      color: Color(0xFF485fa9),
+                      boxShadow: [
+                        new BoxShadow(
+                            color: Color(0x4D000000),
+                            offset: new Offset(0.0, 5.0),
+                            blurRadius: 20.0,
+                            spreadRadius: 1)
+                      ],
+                    ),
+                    height: 200,
+                    width: 120,
+                  ),),
+                  CircleAvatar(
+                    radius: 50,
+                    backgroundColor: Colors.white,
+                  ),
+
+                ],
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
