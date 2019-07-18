ef3675eee8cf5f49306f4d5e089fed14bfc9a65e
Sumit Kumar <sumitskl1999@gmail.com>
2019-07-18 06:19:53 +0530

    production

--- lib/main.dart
+++ lib/main.dart
@@ -25,44 +25,14 @@ class _DashboardState extends State<Dashboard> {
         child: Column(
           children: <Widget>[
             Container(
-              margin: const EdgeInsets.all(10.0),
-              alignment: Alignment.topCenter,
-              child: Text(
-                'Explore',
-                style: TextStyle(
-                  color: Colors.white,
-                  fontSize: 40,
-                ),
-              ),
-            ),
-            Container(
-              height: 50,
-            ),
-            Container(
-              width: 370,
-              alignment: Alignment.centerLeft,
-              child: Text(
-                'Trending',
-                style: TextStyle(
-                  fontSize: 25,
-                  color: Colors.white,
-                ),
-              ),
-            ),
-            Container(
-              height: 10,
-            ),
-            Container(
-              height: 270,
+              height: 750,
               child: PageView(
-                controller: PageController(viewportFraction: 0.9),
                 children: <Widget>[
-                  TCards(),
-                  TCards(),
-                  TCards(),
+                  Objects(),
                 ],
+                scrollDirection: Axis.vertical,
               ),
-            )
+            ),
           ],
         ),
       ),
@@ -70,7 +40,84 @@ class _DashboardState extends State<Dashboard> {
   }
 }
 
-class TCards extends StatelessWidget{
+class Objects extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Column(
+        children: <Widget>[
+          Container(
+            margin: const EdgeInsets.all(10.0),
+            alignment: Alignment.topCenter,
+            child: Text(
+              'Explore',
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 30,
+              ),
+            ),
+          ),
+          Container(
+            height: 20,
+          ),
+          Container(
+            width: 370,
+            alignment: Alignment.centerLeft,
+            child: Text(
+              'Trending',
+              style: TextStyle(
+                fontSize: 25,
+                color: Colors.white,
+              ),
+            ),
+          ),
+          Container(
+            height: 10,
+          ),
+          Container(
+            height: 270,
+            child: PageView(
+              controller: PageController(viewportFraction: 0.9),
+              children: <Widget>[
+                TCards(),
+                TCards(),
+                TCards(),
+              ],
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.only(left: 25.0),
+            child: Container(
+              alignment: Alignment.centerLeft,
+              child: Text(
+                'Latests',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 25,
+                ),
+              ),
+            ),
+          ),
+          SizedBox(height: 10),
+          Container(
+            height: 320,
+            child: PageView(
+              controller: PageController(viewportFraction: 0.9),
+              children: <Widget>[
+                LCards(),
+                LCards(),
+                LCards(),
+              ],
+              scrollDirection: Axis.vertical,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class TCards extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
@@ -133,5 +180,46 @@ class TCards extends StatelessWidget{
       ),
     );
   }
+}
 
+class LCards extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 240,
+      width: 370,
+      child: Stack(
+        children: <Widget>[
+          Container(
+            child: Stack(
+              children: <Widget>[
+                new Align(
+                  alignment: Alignment.center,
+                  child: Container(
+                    decoration: BoxDecoration(
+                      color: Color(0xFF485fa9),
+                      borderRadius: BorderRadius.circular(8.0),
+                      boxShadow: [
+                        new BoxShadow(
+                            color: Color(0x4D000000),
+                            offset: new Offset(0.0, 5.0),
+                            blurRadius: 20.0,
+                            spreadRadius: 1),
+                      ],
+                    ),
+                    height: 200,
+                    width: 370,
+                  ),
+                ),
+              ],
+            ),
+          ),
+          CircleAvatar(
+            backgroundColor: Colors.white,
+            radius: 50,
+          ),
+        ],
+      ),
+    );
+  }
 }
